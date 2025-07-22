36735e16acc03d40b892a58acc432e8743d863ce
Mahmoud <mahmoudabdelkhalik131@gmail.com>
2025-07-22 15:49:52 +0300

    "After solving a very tricky error"

--- src/reviews/review.validation.ts
+++ src/reviews/review.validation.ts
@@ -1,5 +1,7 @@
 import { body, param } from "express-validator";
 import validatorMiddleware from "../middleware/validation.middleware";
+import reviewSchema from "./review.schema";
+import { Schema } from "mongoose";
 
 
 class ReviewsValidation {
@@ -15,9 +17,20 @@ class ReviewsValidation {
       .isFloat({ min: 1, max: 5 })
       .withMessage((val, { req }) => req.__("validation_length_long"))
       ,
+      body("product")
+      .notEmpty().withMessage((val,{ req }) => req.__("validation_field"))
+      .isMongoId().withMessage((val, { req }) => req.__("Invalid Id"))
+      .custom(async(val,{req})=>{
+        const review= await reviewSchema.findOne({product:val,user:req.user._id});
+        if(review){
+          throw new Error(req.__("review_already_exists"));
+        }
+        return true;
+      })
+      ,
     validatorMiddleware,
   ];
-  updateOne = [
+  update = [
     param("id").isMongoId().withMessage((val,{ req }) => req.__("invalid_id")),
     body("comment")
       .optional()
@@ -25,14 +38,19 @@ class ReviewsValidation {
       .withMessage((val, { req }) => req.__("validation_length_long"))
       ,
       body("rate")
-      .optional()
+       .optional()
       .isFloat({ min: 1, max: 5 })
       .withMessage((val, { req }) => req.__("validation_length_long"))
       ,
     validatorMiddleware,
   ];
   deleteOne = [
-    param("id").isMongoId().withMessage((val,{ req }) => req.__("invalid_id")),
+    param("id").isMongoId().withMessage((val,{ req }) => req.__("invalid_id")).custom(async(val,{req})=>{
+      const review = await reviewSchema.findById(val);
+      if(review?.user._id!.toString()!== req.user._id.toString()&& req.user.role!=="admin"){
+        throw new Error(req.__("not_authorized"));
+      } 
+    }),
     validatorMiddleware,
   ];
   getOne = [
